10e920f11087b79de9fff5f04337fddb001222a0
omirostr1 <omiros.trypatsas@gmail.com>
2023-11-14 10:11:23 +0000

    Ensure exhaustiveness for the first two input cases

--- src/main/scala/restaurant_service_charge.scala
+++ src/main/scala/restaurant_service_charge.scala
@@ -1,6 +1,8 @@
 import java.awt.MenuItem
 import scala.io.StdIn.readInt
 import scala.io.StdIn.readLine
+import java.time.LocalDateTime
+import scala.collection.immutable.ListMap
 
 object restaurant_service_charge extends App{
 
@@ -12,11 +14,43 @@ object restaurant_service_charge extends App{
   case object Steak_Sandwich extends MenuItem (4.50, true, true, false)
   case object Lobster extends MenuItem(25, food = true, hot = true, premium = true)
 
+  case class Staff(name: String, wage: BigDecimal, monthsWorking: Int)
+
+  val staff1 = Staff("Omiros", 1000, 1)
+  val staff2 = Staff("Jim", 1500, 4)
+  val staff3 = Staff("Ben", 1000, 2)
+  val staff4 = Staff("Karen", 2000, 14)
+
   var total : BigDecimal = 0
   var tip : BigDecimal = 0
   var foodExists : Boolean = false
   var isHot: Boolean = false
   var isPremium: Boolean = false
+  val happyHour: Boolean = false
+
+  def currency(total: BigDecimal): BigDecimal = {
+    var newTotal: BigDecimal = 0
+    println("Choose the currency of your choice")
+    val currency: Map[Int, String] = Map(1 -> "GBP", 2 -> "EUR", 3 -> "USD", 4 -> "AUD")
+    println(currency)
+    val currencyChoice = readInt()
+    currencyChoice match {
+      case 2 => newTotal = total * 1.14
+      case 3 => newTotal = total * 1.22
+      case 4 => newTotal = total * 1.92
+      case _ => newTotal = total
+    }
+    newTotal
+  }
+
+  def happyHour(cost: BigDecimal, quantity: Int) : BigDecimal = {
+    val now = LocalDateTime.now().getHour
+    if (now >= 18 & now<=20) {
+      (cost) / 2 * quantity
+    } else {
+      cost * quantity
+    }
+  }
 
   def takeOrder(finished: Boolean) : Unit = {
     def accumulator(finished: Boolean) : Unit = {
@@ -33,23 +67,36 @@ object restaurant_service_charge extends App{
         } else {
           tip = 0
         }
+        total = currency(total)
         println(s"The total is $total and the tip suggested is $tip")
+        println(s"The time of transaction is ${LocalDateTime.now().getHour}:${LocalDateTime.now().getMinute}")
       } else {
-        val catalogue: Map[Int, MenuItem] = Map(1 -> Cola, 2 -> Coffee, 3 -> Cheese_Sandwich, 4 -> Steak_Sandwich,
+        val catalogue: ListMap[Int, MenuItem] = ListMap(1 -> Cola, 2 -> Coffee, 3 -> Cheese_Sandwich, 4 -> Steak_Sandwich,
           5 -> Lobster)
         println(catalogue)
         var choice = readInt()
         println(choice)
         println("Please enter quantity")
-        var quantity = readInt()
-        println(quantity)
+        val input = readLine()
+        try {
+          var quantity = input.toInt
+          if (quantity >= 0) {
+            println(quantity)
+          } else {
+            return println("Error! Please enter a valid option.")
+          }
+        } catch {
+          case _: NumberFormatException =>
+            return println("Error! Please enter a valid integer.")
+        }
+        var quantity = input.toInt
         choice match {
-          case 1 => total += catalogue(choice).cost * quantity
-          case 2 => total += catalogue(choice).cost * quantity
+          case 1 => total += happyHour(catalogue(choice).cost, quantity)
+          case 2 => total += happyHour(catalogue(choice).cost, quantity)
           case 3 => total += catalogue(choice).cost * quantity
           case 4 => total += catalogue(choice).cost * quantity
           case 5 => total += catalogue(choice).cost * quantity
-          case _ => println("Error! Please enter a valid option.")
+          case _ => return println("Error! Please enter a valid option.")
         }
         if (catalogue(choice).premium == true) isPremium = true
         if (catalogue(choice).food == true) foodExists = true
